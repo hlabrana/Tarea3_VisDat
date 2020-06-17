814f1e5d434887ac451b68445511ad25dd08d6b7
Nicolas Rosas <nicolas.rosas.gomez@gmail.com>
2020-06-17 19:25:42 -0400

    Grafico con d3plus

--- src/P1_movie.js
+++ src/P1_movie.js
@@ -1,15 +1,15 @@
 var data = [
-    {Geography: "Game of Thrones", Categoria: "Reviews", logy: 3.266, y: "1.682.108"},
-    {Geography: "Sherlock", Categoria: "Reviews", logy: 2.884, y: "766.089"},
-    {Geography: "Death Note", Categoria: "Reviews", logy: 2.327, y: "212.222"},
+    {Geography: "Game of Thrones", Categoria: "Reviews", logvalue: 3.266, value: "1.682.108"},
+    {Geography: "Sherlock", Categoria: "Reviews", logvalue: 2.884, value: "766.089"},
+    {Geography: "Death Note", Categoria: "Reviews", logvalue: 2.327, value: "212.222"},
 
-    {Geography: "Game of Thrones", Categoria: "Ranking", logy: 0.602, y: 4},
-    {Geography: "Sherlock", Categoria: "Ranking", logy: 2.061, y: 115},
-    {Geography: "Death Note", Categoria: "Ranking", logy: 2.640, y: 437},
+    {Geography: "Game of Thrones", Categoria: "Ranking", logvalue: 0.602, value: 4},
+    {Geography: "Sherlock", Categoria: "Ranking", logvalue: 2.061, value: 115},
+    {Geography: "Death Note", Categoria: "Ranking", logvalue: 2.640, value: 437},
 
-    {Geography: "Game of Thrones", Categoria: "Rating", logy: 0.968, y: "9.3"},
-    {Geography: "Sherlock", Categoria: "Rating", logy: 0.959, y: "9.1"},
-    {Geography: "Death Note", Categoria: "Rating", logy: 0.954, y: "9.0"}
+    {Geography: "Game of Thrones", Categoria: "Rating", logvalue: 0.968, value: "9.3"},
+    {Geography: "Sherlock", Categoria: "Rating", logvalue: 0.959, value: "9.1"},
+    {Geography: "Death Note", Categoria: "Rating", logvalue: 0.954, value: "9.0"}
 ];
 
 d3.select("#GotChart")
@@ -24,7 +24,7 @@ new d3plus.Radar()
 data: data,
 groupBy: "Categoria",
 metric: "Geography",
-y: "logy",
+value: "logvalue",
 axisConfig: {
     shapeConfig: {
     labelConfig: {
@@ -37,7 +37,7 @@ axisConfig: {
 },
 tooltipConfig: {
     tbody: [
-        ["Valor: ", function(d) { return d["y"] }],
+        ["Valor: ", function(d) { return d["value"] }],
         ["Año: ", function(d) { return 2020 }]
     ]
 }

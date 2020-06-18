40b0af1261ea6d0720ffdf6bc3f8a966b33e99c2
vyagui <vicente.yagui@sansano.usm.cl>
2020-06-17 20:41:08 -0400

    aaa

--- src/P3_music.js
+++ src/P3_music.js
@@ -1,9 +1,16 @@
 var data = [
-  {parent: "Group 1", id: "alpha", value: 29},
-  {parent: "Group 1", id: "beta", value: 10},
-  {parent: "Group 1", id: "gamma", value: 2},
-  {parent: "Group 2", id: "delta", value: 29},
-  {parent: "Group 2", id: "eta", value: 25}
+  {parent: "Bad Bunny", id: "LAS QUE NO IBAN A SALIR", value:81.60},
+  {parent: "Bad Bunny", id: "YHLQMDLG", value: 81.45},
+  {parent: "Bad Bunny", id: "OASIS", value: 74.25},
+  {parent: "Bad Bunny", id: "X 100PRE", value: 70.40},
+  {parent: "Drake", id: "Dark Lane Demo Tapes", value:79.36},
+  {parent: "Drake", id: "Scorpion", value: 66.92},
+  {parent: "Drake", id: "More Life", value: 64.50},
+  {parent: "Drake", id: "Views", value: 64.36}
+  {parent: "Hilary Hahn", id: "Hilary Hahn plays Bach: Violin Sonatas Nos. 1 & 2; Partita No. 1", value:28.81},
+  {parent: "Hilary Hahn", id: "Mozart: Violin Sonatas K.301, 304, 376 & 526", value: 25.16},
+  {parent: "Hilary Hahn", id: "J.S.Bach: Violin Concertos", value: 22.83},
+  {parent: "Hilary Hahn", id: "Hilary Hahn Plays Bach", value: 29.88}
 ];
   
 d3.select("#BBBABYY")

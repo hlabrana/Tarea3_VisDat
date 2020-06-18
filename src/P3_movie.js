3b42f0466fc01c79ab5b8900a3e141622de19b58
hlabrana <hectorlabranarojas@gmail.com>
2020-06-17 22:22:26 -0400

    PANEL 3: modify x/y axes in movie chart

--- src/P3_movie.js
+++ src/P3_movie.js
@@ -1,10 +1,10 @@
 var data = [
-    {id: "Game of Thrones [Tomatoes]", x: 4, y:  7, value: 89, name:'Game of Thrones'},
-    {id: "Sherlock [Tomatoes]",  x: 5.6, y:  2, value: 78, name:'Sherlock'},
-    {id: "Death Note [Tomatoes]", x: 5, y:  7, value: 100, name:'Death Note'},
-    {id: "Game of Thrones [Popcorn]",  x: 5, y:  21, value: 86, name:'Game of Thrones'},
-    {id: "Sherlock [Popcorn]", x: 6, y: 21, value: 83, name:'Sherlock'},
-    {id: "Death Note [Popcorn]", x: 6, y: 13, value: 95, name:'Death Note'}
+    {id: "Game of Thrones [Tomatoes]", x: 1, y: 235000, value: 89, name:'Game of Thrones'},
+    {id: "Sherlock [Tomatoes]",  x: 2, y: 730234, value: 78, name:'Sherlock'},
+    {id: "Death Note [Tomatoes]", x: 3, y: 560000, value: 100, name:'Death Note'},
+    {id: "Game of Thrones [Popcorn]",  x: 4, y:  210000, value: 86, name:'Game of Thrones'},
+    {id: "Sherlock [Popcorn]", x: 5, y: 350000, value: 83, name:'Sherlock'},
+    {id: "Death Note [Popcorn]", x: 6, y: 500000, value: 95, name:'Death Note'}
 ];
 
 
